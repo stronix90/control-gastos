914356719169bde1dcd40384d59d20284eab6d6b
Brian Luna <www.correo.com@gmail.com>
2023-12-18 00:28:48 -0300

    ajustes en diseño del Resumen

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+import { Card } from "react-bootstrap";
 import { useGeneral } from "../context/generalContext";
 
 export default function Resume() {
@@ -26,11 +28,73 @@ export default function Resume() {
       </div>
 
       <div className="card-body">
-        <p>
-          <b>Gasto total: ${convertToMoney(resume.total)}</b>
-        </p>
+        <div
+          style={{
+            display: "flex",
+            overflowX: "auto",
+            scrollSnapType: "x mandatory",
+            whiteSpace: "nowrap",
+            gap: "1rem",
+            justifyContent: "space-between",
+          }}
+        >
+          <Card
+            className="bg-dark text-white"
+            style={{
+              flex: "0 0 300px",
+              boxSizing: "border-box",
+              scrollSnapAlign: "center",
+              minHeight: "200px",
+            }}
+          >
+            <Card.Body>
+              <Card.Title>Gasto total</Card.Title>
+              <Card.Text>${convertToMoney(resume.total)}</Card.Text>
+            </Card.Body>
+          </Card>
 
-        <table class="table-dark w-100">
+          {Object.keys(resume.totalPerUser).map((persona) => {
+            const gasto = resume.totalPerUser[persona];
+            const porcentaje = porcentajes[persona];
+            const corresponde = (resume.total * porcentajes[persona]) / 100;
+            const diferencia = corresponde - gasto;
+
+            return (
+              <Card
+                key={persona}
+                className="bg-dark text-white"
+                style={{
+                  flex: "0 0 300px",
+                  boxSizing: "border-box",
+                  scrollSnapAlign: "center",
+                  minHeight: "200px",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>{persona}</Card.Title>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      gap: "1rem",
+                    }}
+                  >
+                    <p style={{ margin: 0 }}>Gastó: ${convertToMoney(gasto)}</p>
+                    <p style={{ margin: 0 }}>
+                      {porcentaje} del total: ${convertToMoney(corresponde)}
+                    </p>
+                    <p style={{ margin: 0 }}>
+                      Diferencia: ${convertToMoney(diferencia)}
+                    </p>
+                  </div>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+
+        {/* <table class="table-dark w-100">
           <thead>
             <tr>
               <th scope="col">Usuario</th>
@@ -58,7 +122,7 @@ export default function Resume() {
               );
             })}
           </tbody>
-        </table>
+        </table> */}
       </div>
     </div>
   );
