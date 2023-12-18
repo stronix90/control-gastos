019aeca27bfdc2deb8bf9720d26301f44bd211dc
Brian Luna <www.correo.com@gmail.com>
2023-12-18 20:53:08 -0300

    records css updated

--- src/components/Records.js
+++ src/components/Records.js
@@ -21,11 +21,67 @@ const Records = ({ record, openModal }) => {
   };
 
   return (
+    <>
+      <div className="row record">
+        {/* // Avatars */}
+        <div className="col-auto my-auto d-flex flex-column align-items-center ps-0">
+          <div className="mainAvatar">{iniciales}</div>
+          <div className="secondAvatar">{record.icon}</div>
+        </div>
+
+        {/* // 2da columna */}
+        <div className="col px-0 details_column">
+          <p className="text_primary">
+            {record.description ? `${record.description}` : record.categoria}
+          </p>
+
+          <div className="details_extra">
+            <p className="text_secondary">
+              {record.description
+                ? daysName[new Date(record.date).getDay()] +
+                  " " +
+                  new Date(record.date).getDate() +
+                  " | " +
+                  record.categoria
+                : daysName[new Date(record.date).getDay()] +
+                  " " +
+                  new Date(record.date).getDate()}
+            </p>
+            <p className="text_amount">${record.monto}</p>
+          </div>
+        </div>
+
+        {/* // Acciones */}
+        {/* <div className="col-auto my-auto ms-auto">
+        <i
+          style={{ cursor: "pointer" }}
+          className="fa fa-trash"
+          aria-hidden="true"
+          onClick={delEntry}
+        ></i>
+        <i
+          style={{ cursor: "pointer", marginLeft: "10px" }}
+          className="fa fa-edit"
+          aria-hidden="true"
+          onClick={editEntry}
+        ></i>
+      </div> */}
+
+        {/* // Monto */}
+        {/* <div className="col-auto my-auto text_amount">${record.monto}</div> */}
+      </div>
+    </>
+  );
+
+  return (
     <div className="row">
-      <div className="col-auto my-auto">
+      {/* // Avatars */}
+      <div className="col-auto my-auto d-flex flex-column align-items-end ">
         <div className="mainAvatar">{iniciales}</div>
         <div className="secondAvatar">{record.icon}</div>
       </div>
+
+      {/* // Descripción */}
       <div className="col my-auto px-0">
         <p>{record.description ? `${record.description}` : record.categoria}</p>
 
@@ -41,6 +97,8 @@ const Records = ({ record, openModal }) => {
               new Date(record.date).getDate()}
         </p>
       </div>
+
+      {/* // Acciones */}
       <div className="col-auto my-auto ms-auto">
         <i
           style={{ cursor: "pointer" }}
@@ -55,6 +113,8 @@ const Records = ({ record, openModal }) => {
           onClick={editEntry}
         ></i>
       </div>
+
+      {/* // Monto */}
       <div className="col-auto my-auto text_amount">${record.monto}</div>
     </div>
   );
